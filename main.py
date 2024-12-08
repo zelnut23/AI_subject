fbec93bb6c9b81110b373c6e6c46704d743de836
zelnut23 <zelnut00@gmail.com>
2024-12-08 22:57:18 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from langchain_openai import ChatOpenAI
+chat_model = ChatOpenAI()
+
+import streamlit as st
+
+st.title("인공지능 시인")
+subject = st.text_input("시의 주제를 입력해주세요.")
+st.write("시의 주제 : " + subject)
+
+if st.button("시 작성"):
+    with st.spinner("시 작성중 ..."):
+        result = chat_model.invoke(subject + "에 대한 시를 써줘")
+        st.write(result.content)
